80d9282149876cdd060dbc92739ec2f6e4859c12
HeatherFields <heather.fields03@gmail.com>
2024-10-08 08:25:36 -0500

    updates

---
+++ venvidi/Scripts/YEA.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+file_path = "S:\Fundraising\Appeal Letters\Year End Appeal\2024\YEA_2024_Masterlist.xlsx"
+data = pd.read_excel("S:\Fundraising\Appeal Letters\Year End Appeal\2024\YEA_2024_Masterlist.xlsx")
